20f2c418804c28fe3bc0c8c8254d8dbc194d6cd0
vc-kathan <kathan.patel@viitor.cloud>
2023-08-13 22:35:02 +0530

    - Added UI to show product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [NgFor]
 })
-export class AppComponent {
-  title = 'angular-pwa-app';
+export class AppComponent implements OnInit {
+  productList: any;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.http.get('https://fakestoreapi.com/products').subscribe({
+      next: (products) => {
+        this.productList = products;
+        console.log("productList =>", this.productList);
+      }
+    })
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
+    .catch(err => console.error(err));
